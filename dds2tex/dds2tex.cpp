dbdec9a5652969e2ea0b1501cad0789b268885ae
Bryan Rykowski <90996715+BryanRykowski@users.noreply.github.com>
2023-10-15 14:34:01 -0400

    Continue work on ug2-dds2tex.
    Program now creates full tex.xbx files, but it's unclear how checksums
    are supposed to be generated.

--- dds2tex/dds2tex.cpp
+++ dds2tex/dds2tex.cpp
@@ -6,12 +6,15 @@
 #include "../common/dds_header.hpp"
 #include "../common/write_word.hpp"
 #include "../common/read_word.hpp"
+#include "../common/tex_header.hpp"
+#include "../common/crc.hpp"
 
 typedef std::vector<std::filesystem::path> FileList;
 
 struct OptionStruct
 {
 	bool write = true;
+	bool quiet = false;
 };
 
 bool ReadArgs(int argc, char **argv, std::filesystem::path &out_path, FileList &file_list, OptionStruct &options);
@@ -73,6 +76,10 @@ bool ReadArgs(int argc, char **argv, std::filesystem::path &out_path, FileList &
 				else if (c == 'n')
 				{
 					options.write = false;
+				}
+				else if (c == 'q')
+				{
+					options.quiet = true;
 				}
 			}
 		}
@@ -146,19 +153,91 @@ bool ReadDdsHeader(std::ifstream &in_stream, DdsFileHeader &dds_header)
 	return false;
 }
 
-bool SkipDdsLevels(std::ifstream &in_stream, const DdsFileHeader &dds_header)
+bool GetDdsData(std::ifstream &in_stream, std::vector<char> &dds_data, const DdsFileHeader &dds_header)
 {
+	unsigned int total_size = 0;
+	unsigned int level_size = dds_header.pitch;
+
 	for (unsigned int i = 0; i < dds_header.levels; ++i)
 	{
+		total_size += level_size;
+		level_size /= 4;
+	}
 
+	dds_data.resize(total_size);
+
+	in_stream.read(dds_data.data(), total_size);
+
+	if (in_stream.fail())
+	{
+		std::cerr << "Error: Failed to read dds pixel data" << std::endl;
+		return true;
 	}
 	
+	return false;
+}
+
+bool WriteImageHeader(std::ofstream &out_stream, TexImageHeader &image_header)
+{
+	char buffer[32];
+
+	write_u32le(buffer, image_header.checksum);
+	write_u32le(buffer + 4, image_header.width);
+	write_u32le(buffer + 8, image_header.height);
+	write_u32le(buffer + 12, image_header.levels);
+	write_u32le(buffer + 16, 32); // Don't know what these are. Often 32.
+	write_u32le(buffer + 20, 32);
+	write_u32le(buffer + 24, image_header.dxt);
+	write_u32le(buffer + 28, 0);
+
+	out_stream.write(buffer, 32);
+
+	if (out_stream.fail())
+	{
+		std::cerr << "Error: Failed to write image file header" << std::endl;
+		return true;
+	}
+
+	return false;
+}
+
+bool WriteImageData(std::ofstream &out_stream, std::vector<char> &image_data, DdsFileHeader &dds_header)
+{
+	unsigned int level_size = dds_header.pitch;
+	char level_size_le[4];
+	unsigned int data_pos = 0;
+
+	for (unsigned int i = 0; i < dds_header.levels; ++i)
+	{
+		write_u32le(level_size_le, level_size);
+
+		out_stream.write(level_size_le, 4);
+
+		if (out_stream.fail())
+		{
+			std::cerr << "Error: Failed to write image file data" << std::endl;
+			return true;
+		}
+
+		out_stream.write(&image_data[data_pos], level_size);
+
+		if (out_stream.fail())
+		{
+			std::cerr << "Error: Failed to write image file data" << std::endl;
+			return true;
+		}
+
+		data_pos += level_size;
+		level_size /= 4;
+	}
+
 	return false;
 }
 
 bool ReadFiles(std::filesystem::path &out_path, FileList &file_list, OptionStruct &options)
 {
 	std::ofstream out_stream;
+	std::vector<char> dds_data;
 
 	if (options.write)
 	{
@@ -177,6 +256,7 @@ bool ReadFiles(std::filesystem::path &out_path, FileList &file_list, OptionStruc
 	{
 		std::ifstream in_stream(file_list[i], std::ios::binary);
 		DdsFileHeader dds_header;
+		TexImageHeader image_header;
 
 		if (in_stream.fail())
 		{
@@ -186,7 +266,54 @@ bool ReadFiles(std::filesystem::path &out_path, FileList &file_list, OptionStruc
 
 		if (ReadDdsHeader(in_stream, dds_header)) return true;
 
-		if (SkipDdsLevels(in_stream, dds_header)) return true;
+		if (!options.quiet)
+		{
+			std::cout << file_list[i].string() << std::endl;
+			std::cout << "width: " << dds_header.width << std::endl;
+			std::cout << "height: " << dds_header.height << std::endl;
+			std::cout << "dxt: " << dds_header.pix_fmt.fourcc[3] << std::endl;
+			std::cout << "mipmap levels: " << dds_header.levels << std::endl << std::endl;
+		}
+
+		if (options.write)
+		{
+			if (GetDdsData(in_stream, dds_data, dds_header)) return true;
+			
+			image_header.checksum = BufferCRC(dds_data.data(), dds_data.size());
+			image_header.width = dds_header.width;
+			image_header.height = dds_header.height;
+			image_header.levels = dds_header.levels;
+
+			switch (dds_header.pix_fmt.fourcc[3])
+			{
+				case '1':
+					image_header.dxt = 1;
+					break;
+
+				case '2':
+					image_header.dxt = 2;
+					break;
+					
+				case '3':
+					image_header.dxt = 3;
+					break;
+
+				case '4':
+					image_header.dxt = 4;
+					break;
+
+				case '5':
+					image_header.dxt = 5;
+					break;
+			
+				default:
+					std::cerr << "Error: DDS file unsupported fourcc \"" << dds_header.pix_fmt.fourcc << "\"" << std::endl;
+					return true;
+			}
+
+			if (WriteImageHeader(out_stream, image_header)) return true;
+			if (WriteImageData(out_stream, dds_data, dds_header)) return true;
+		}
 	}
 
 	return false;
